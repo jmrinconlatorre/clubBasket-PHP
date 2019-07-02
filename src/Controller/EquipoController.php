be7cddc364746ff1606db7d907db65838635a4a0
juanma <jm.rincon.latorre@gmail.com>
2019-07-02 16:35:21 +0200

    EquipoController created

---
+++ src/Controller/EquipoController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Equipo;
+
+/**
+ * @Route("/equipo")
+ */
+class EquipoController extends AbstractController
+{
+    /**
+     * @Route("/lista",name="listaEquipo")
+     */
+    public function listaEquipos()
+    {
+        $repository = $this->getDoctrine()->getRepository(Equipo::class);
+        $equipos = $repository->findAll();
+        return $this->render(
+            'equipo/lista.html.twig',
+            ['equipos'=>$equipos]
+        );
+    }
+}
